2d9ee6612885c894ad14c18f4a3bd606d69c0aae
ext.marcelo.carvalho <ext.marcelo.carvalho@farfetch.com>
2019-06-11 17:18:29 +0100

    Adding functions to read the XML struct files

---
+++ Kundt/Repository/XMLData/XML.Struct.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+
+namespace Repository.XMLData
+{
+    public class XMLStruct
+    {
+
+        public Dictionary<string,string> GetStructureFiles()
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            string pathXMLFiles = FullPathXMLFiles();          
+            foreach (XElement files in XElement.Load(pathXMLFiles).Elements("File"))
+            {
+                result.Add(files.Attribute("name").Value, files.Attribute("Local").Value);
+            }
+            return result;
+
+        }
+
+        public Dictionary<string, int> GetDataIndex()
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            string pathXMLFiles = FullPathXMLFiles();
+            //....
+            //line start
+            //line end
+            //StructLine
+            //....
+            //foreach (XElement files in XElement.Load(pathXMLFiles).Elements("File"))
+            //{
+            //    result.Add(files.Attribute("name").Value, files.Attribute("Local").Value);
+            //}
+            return result;
+
+        }
+
+        private string FullPathXMLFiles(bool forceCreate = true)
+        {
+            string result;
+            result = AppDomain.CurrentDomain.BaseDirectory +  @"\XML\StructureFiles.xml";
+
+            if (!Directory.Exists(result) && forceCreate)
+            {
+                Directory.CreateDirectory(result);
+                CreateStructDefaultFile();
+            }
+
+            result = result + @"\StructureFiles.xml";
+            if (!File.Exists(result))
+            {
+                if (forceCreate)
+                {
+                    CreateStructDefaultFile();
+                }
+                else
+                {
+                    throw new FileNotFoundException("Structure File not found. Error XML call.");
+                }
+            }
+
+            return @result;
+        }
+
+        private void CreateStructDefaultFile()
+        {
+            //TODO:
+        }
+        // GetResourceTextFile("myXmlDoc.xml")
+
+        /*
+		 
+		 public string GetResourceTextFile(string filename)
+			{
+				string result = string.Empty;
+
+				using (Stream stream = this.GetType().Assembly.
+						   GetManifestResourceStream("assembly.folder."+filename))
+				{
+					using (StreamReader sr = new StreamReader(stream))
+					{
+						result = sr.ReadToEnd();
+					}
+				}
+				return result;
+			}
+		 
+		 
+		 */
+    }
+}
---
+++ Kundt/UnitTest/Gateway/Repository/XMLTest.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Repository.XMLData; 
+
+    [TestClass]
+    public class XMLTest
+    {
+        [TestMethod]
+        public void LoadFileStruct_Ok()
+        {
+            //Arrange
+            XMLStruct xmlStruct = new XMLStruct();
+            
+            //Act
+            var test = xmlStruct.GetStructureFiles();
+
+            //Assert
+            Assert.AreEqual(1, test.Count);
+        }
+    }
+}
+
+
